35621da2eea5171728eb1c75968e385e5f9c2c8a
stanly laptop <stanly61315@gmail.com>
2021-06-19 00:02:23 +0800

    v1

--- main.py
+++ main.py
@@ -1,11 +1,34 @@
 import os
-from flask import Flask
+from flask import Flask, request
+import requests
+from dhooks import Webhook, File
 
 app = Flask(__name__)
+env = os.environ
 
 
-@app.route("/")
+@app.route("/", methods=["POST"])
 def webhook():
+    content = request.json["content"]
+    username = request.json["username"]
+    avatar_url = request.json["avatar_url"]
+    media = request.json["media"].split(",")
+    webhook_url = (
+        "https://discord.com/api/webhooks/"
+        + env.get("webhook_id")
+        + "/"
+        + env.get("webhook_token")
+    )
+    hook = Webhook(webhook_url)
+
+    hook.send(content=content, username=username, avatar_url=avatar_url)
+
+    for url in media:
+        r = requests.get(url)
+        open("file", "wb").write(r.content)
+        f = File("file", "image.jpg")
+        hook.send(file=f, username=username, avatar_url=avatar_url)
+
     return "200 ok"
 
 
