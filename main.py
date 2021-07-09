c637d7b104edbc61f803c201a4a30473ba178fcb
stanly0726 <37040069+stanly0726@users.noreply.github.com>
2021-07-09 23:03:57 +0800

    delay between message

--- main.py
+++ main.py
@@ -30,6 +30,7 @@ def webhook():
     release_date = 1642694400
     now = Time()
     time = "> " + str(round((release_date - now) / 86400)) + " day until release"
+    time.sleep(1)
 
     hook.send(content=time, username=username, avatar_url=avatar_url)
     hook.send(content=content, username=username, avatar_url=avatar_url)
