1b02cfa4e3e4ec28f13803c68c951043e91e6060
stanly0726 <37040069+stanly0726@users.noreply.github.com>
2021-07-09 23:04:23 +0800

    ..

--- main.py
+++ main.py
@@ -30,9 +30,9 @@ def webhook():
     release_date = 1642694400
     now = Time()
     time = "> " + str(round((release_date - now) / 86400)) + " day until release"
-    time.sleep(1)
 
     hook.send(content=time, username=username, avatar_url=avatar_url)
+    time.sleep(1)
     hook.send(content=content, username=username, avatar_url=avatar_url)
     try:
         for url in media:
